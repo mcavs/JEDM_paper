79d37a0114256b58f1afeb0307a08306048a0fbe
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-11-28 20:42:03 +0300

    Add files via upload

---
+++ process_binary.R
@@ -0,0 +1,11 @@
+data <- dataset |>
+  drop_na() |>
+  filter(final_result != "Withdrawn") |>
+  select(gender, region, highest_education, imd_band, age_band, disability, final_result) |>
+  mutate(age_band          = as.numeric(as.factor(age_band))) |>
+  mutate(imd_band          = as.numeric(as.factor(imd_band))) |>
+  mutate(highest_education = as.numeric(as.factor(highest_education))) |>
+  mutate(region            = as.numeric(as.factor(region))) |>
+  mutate(gender            = as.numeric(as.factor(gender))) |>
+  mutate(disability        = as.numeric(as.factor(disability))) |>
+  mutate(final_result      = ifelse(final_result == "Fail", 1, 0)) 
