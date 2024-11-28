62ac464a31531f758e99b6d6f77afc88ca9c08a0
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-11-28 20:41:13 +0300

    Add files via upload

---
+++ data_import.R
@@ -0,0 +1,9 @@
+courseA <- read_csv("data/A.csv")
+courseB <- read_csv("data/B.csv")
+courseD <- read_csv("data/D.csv")
+courseE <- read_csv("data/E.csv")
+
+courseG_2013 <- read_csv("data/G_2013.csv")
+courseG_2014 <- read_csv("data/G_2014.csv")
+courseF_2013 <- read_csv("data/F_2013.csv")
+courseF_2014 <- read_csv("data/F_2014.csv")
